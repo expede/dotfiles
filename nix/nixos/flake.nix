74da0cbea54eedf716f9209fd60ef3e8d3258352
Brooklyn Zelenka <hello@brooklynzelenka.com>
2025-08-16 13:27:45 -0700

    Hibernate working!

--- nix/nixos/flake.nix
+++ nix/nixos/flake.nix
@@ -2,10 +2,10 @@
   description = "expede's NixOS Configuration";
 
   inputs = {
-    nixpkgs.url       = "github:nixos/nixpkgs";
+    nixpkgs.url       = "github:nixos/nixpkgs/nixos-25.05";
     unstable-pkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-    home-manager.url                    = "github:nix-community/home-manager";
+    home-manager.url                    = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
